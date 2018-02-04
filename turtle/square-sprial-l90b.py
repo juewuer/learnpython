1948a0cffc460ca8c2788995f0d639aafdf1f027
li hechao <lihechao@kingsoft.com>
2018-02-04 18:46:20 +0800

    第一个turtle程序

---
+++ turtle/square-sprial-l90b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/4 18:32
+# @Author  : 
+# @Site    : 
+# @File    : square-sprial-l90b.py.py
+# @copyright: ksyun
+
+import turtle
+t = turtle.Pen()
+for x in range(100):
+    t.forward(x)
+    t.left(90)
